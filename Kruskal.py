9829c2e687196a8949a45471565bf6cd5a9efbe0
NehaKulkarni28 <158441391+Nehak89@users.noreply.github.com>
2025-04-27 13:10:27 +0530

    Kruskal.py

--- Kruskal.py
+++ Kruskal.py
@@ -1,25 +1,37 @@
 class UnionFind:
-    def __init__(self, n): self.parent = list(range(n))
-    def find(self, x): return x if self.parent[x] == x else self.find(self.parent[x])
-    def union(self, x, y): self.parent[self.find(x)] = self.find(y)
+    def __init__(self, nodes):
+        self.parent = {node: node for node in nodes}  # parent dictionary for string nodes
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        self.parent[self.find(x)] = self.find(y)
 
 def kruskal(graph):
     edges = [(w, u, v) for u in graph for v, w in graph[u]]
     edges.sort()
-    uf = UnionFind(len(graph))
+    uf = UnionFind(graph.keys())
     mst = []
     for w, u, v in edges:
         if uf.find(u) != uf.find(v):
             uf.union(u, v)
             mst.append((u, v, w))
     return mst
-
+    
 n = int(input("Enter number of edges: "))
 graph = {}
+
 for _ in range(n):
-    u, v, cost = input().split()
+    u, v, cost = input("Enter edge (u v cost): ").split()
     cost = int(cost)
     graph.setdefault(u, []).append((v, cost))
     graph.setdefault(v, []).append((u, cost))
 
-print("Kruskal's MST:", kruskal(graph))
+mst = kruskal(graph)
+
+print("Kruskal's MST:")
+for u, v, w in mst:
+    print(f"{u} - {v}: {w}")
