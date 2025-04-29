bf2275955440d1c6f6c93730f93c3ac96231c90e
core012 <167705440+core012@users.noreply.github.com>
2025-04-30 00:15:18 +0530

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,65 +1,26 @@
+def get_bot_response(user_input):
+    user_input = user_input.lower()
+    
+    if "hello" in user_input or "hi" in user_input:
+        return "Hello! How can I assist you today?"
+    elif "how are you" in user_input:
+        return "I'm just a bot, but I'm doing great!"
+    elif "your name" in user_input:
+        return "I'm ChatBot, your friendly assistant."
+    elif "bye" in user_input:
+        return "Goodbye! Have a nice day!"
+    else:
+        return "I'm not sure how to respond to that."
 
-def greet(bot_name, birth_year):
-    print ('Hello! My name is {0}.'.format(bot_name))
-    print ('I was created in {0}.'.format(birth_year))
+def chat():
+    print("Welcome to ChatBot! Type 'bye' to exit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "bye":
+            print("ChatBot: Goodbye! 👋")
+            break
+        response = get_bot_response(user_input)
+        print("ChatBot:", response)
 
-
-def remind_name():
-    print ('Please, remind me your name.')
-    name = input()
-    print ('What a great name you have, {0}!'.format(name))
-
-
-def guess_age():
-    print ('Let me guess your age.')
-    print ('Enter remainders of dividing your age by 3, 5 and 7.')
-
-    rem3 = int(input())
-    rem5 = int(input())
-    rem7 = int(input())
-    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-
-    print ("Your age is {0}; that's a good time to start programming!".format(age))
-
-
-def count():
-    print ('Now I will prove to you that I can count to any number you want.')
-    num = int(input())
-    counter = 0
-    while counter <= num:
-        print ('{0} !'.format(counter))
-        counter += 1
-
-
-def test():
-    print ("Let's test your programming knowledge.")
-    print ('Why do we use methods?')
-    print ('1. To repeat a statement multiple times.')
-    print ('2. To decompose a program into several small subroutines.')
-    print ('3. To determine the execution time of a program.')
-    print ('4. To interrupt the execution of a program.')
-
-    answer = 2
-    guess = int(input())
-    while guess != answer:
-        print ('Please, try again.')
-        guess = int(input())
-
-    print ('Completed, have a nice day!')
-    print ('.................................')
-    print ('.................................')
-    print ('.................................')
-
-def end():
-    print ('Congratulations, have a nice day!')
-    print ('.................................')
-    print ('.................................')
-    print ('.................................')
-    input()
-
-greet('ChatbotA', '2025')
-remind_name()
-guess_age()
-count()
-test()
-end()
+# Run the chatbot
+chat()
